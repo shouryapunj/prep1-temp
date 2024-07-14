7622ae15d1186fa2b2350cbcb0a1c3e55ebf9ec8
Shourya Punj <shouryapunj93@gmail.com>
2024-07-13 20:35:04 -0500

    XYZ-1: add boilerplate code

---
+++ src/main/java/dev/intuit/prep1/Prep1Application.java
@@ -0,0 +1,13 @@
+package dev.intuit.prep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prep1Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/intuit/prep1/Prep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.intuit.prep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
